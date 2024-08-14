eb02ee48e83ec998a147deefd9a4964aa40be1b2
Mehdi Rahman <mehdirahbvd@gmail.com>
2024-08-14 16:52:48 +0200

    Fill missing dirs and jpeg filenames when filling the db from the legacy one

--- PicturesDjango/PicturesApp/management/commands/FillFromLegacyCppDb.py
+++ PicturesDjango/PicturesApp/management/commands/FillFromLegacyCppDb.py
@@ -1,9 +1,46 @@
 import struct
-
-from django.db import models
+import os
 from django.core.management.base import BaseCommand
+from PicturesApp.PhotoModel import PhotoModel
 
-from  PicturesApp.PhotoModel import PhotoModel
+'''Import old db
+Nightmarres found:
+i) in some cases, there is a jpeg but file name is not set, it must be completed in an incremental way when agrandi is true,
+starting at 001.jpg, with this formatting nom_fichier_jpeg = f"{the_number:03d}.jpg"
+2) in some cases,  directories are not given. What we have is a batch named preparehtml.bat which internally reference the subject.
+So by having batch location for a given subject, we can find the directories.
+3) agrandi can very well means paper version, but not necessarily a jpeg version. So if camera_digitale is not set, the batch
+presence is requiered to be sure that pictures does indeed really exists.
+'''
+
+
+def scan_for_batches(root_dir):
+    subject_to_paths = {}
+    current_jpeg_count = {}
+
+    def recursive_scan(path, levels):
+        for entry in os.listdir(path):
+            entry_path = os.path.join(path, entry)
+            if os.path.isdir(entry_path):
+                if levels < 3:
+                    recursive_scan(entry_path, levels + 1)
+            elif entry == 'preparehtml.bat':
+                with open(entry_path, 'r', encoding='cp1252') as file:
+                    for line in file:
+                        if 'prephtml' in line:
+                            subject = line.split('"')[1].lower()
+                            relative_path = os.path.relpath(path, root_dir)
+                            folders = relative_path.split(os.sep)
+                            subject_to_paths[subject] = {
+                                'premier_niveau': folders[0] if len(folders) > 0 else '',
+                                'second_niveau': folders[1] if len(folders) > 1 else '',
+                                'troisieme_niveau': folders[2] if len(folders) > 2 else ''
+                            }
+                            current_jpeg_count[subject] = 1
+
+    recursive_scan(root_dir, 0)
+    return subject_to_paths, current_jpeg_count
+
 
 def read_null_terminated_string(f, encoding='ISO-8859-1'):
     chars = []
@@ -14,7 +51,12 @@ def read_null_terminated_string(f, encoding='ISO-8859-1'):
         chars.append(char)
     return b''.join(chars).decode(encoding)
 
+
 def read_binaire_file(filename):
+    #load the batches to have the relation between subject and directories where pictures are located
+    root_dir = '/home/mehdi/Images'
+    subject_paths, jpeg_count = scan_for_batches(root_dir)
+
     with open(filename, 'rb') as f:
         # Lire la version (4 octets) et le nombre de records (4 octets)
         version, num_records = struct.unpack('II', f.read(8))
@@ -43,6 +85,16 @@ def read_binaire_file(filename):
             second_niveau = read_null_terminated_string(f)
             troisieme_niveau = read_null_terminated_string(f)
             nom_fichier_jpeg = read_null_terminated_string(f)
+
+            #fill missing directories and jpeg name, see comment at top of this file
+            if not nom_fichier_jpeg and agrandi:
+                if sujet.lower() in subject_paths:
+                    nom_fichier_jpeg = f"{jpeg_count[sujet.lower()]:03d}.jpg"
+                    jpeg_count[sujet.lower()] = jpeg_count[sujet.lower()] + 1
+                    if not premier_niveau:
+                        premier_niveau = subject_paths[sujet.lower()]['premier_niveau']
+                        second_niveau = subject_paths[sujet.lower()]['second_niveau']
+                        troisième_niveau = subject_paths[sujet.lower()]['troisieme_niveau']
 
             # Préparer le dictionnaire des données
             photo_data = {
@@ -74,13 +126,13 @@ def save_photos_to_db(photos):
         photo = PhotoModel(**photo_data)
         photo.save()
 
+
 class Command(BaseCommand):
     help = (
         'Convert my very legacy format from the nineties where db was composed of nul terminated C strings, '
         'in western european.'
         'Need the db file, ie  /home/mehdi/Images/dbase/SQLBASE/Dias.bdd'
         'So syntax is python manage.py FillFromLegacyCppDb --DiasbddPath /home/mehdi/Images/dbase/SQLBASE/Dias.bdd')
-
 
     def add_arguments(self, parser):
         parser.add_argument('--DiasbddPath', type=str, help='Required, ie /home/mehdi/Images/dbase/SQLBASE/Dias.bdd')
