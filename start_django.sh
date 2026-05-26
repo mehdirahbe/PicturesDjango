90fe9e1d4659a55ec6270e97b428f971fd95aaa6
Mehdi Rahman <mehdirahbvd@gmail.com>
2026-05-26 17:09:20 +0200

    Add auto resize of image in the navigator+in the shell script to launch unicorn, kill residues of last run

--- start_django.sh
+++ start_django.sh
@@ -3,11 +3,15 @@
 # Aller dans le répertoire du projet (optionnel si tu l'exécutes déjà depuis là, mais utile pour la portabilité)
 cd "$(dirname "$0")"  # Ça place le script dans son propre répertoire
 
+# clean residues from previous run
+pkill -9 -f gunicorn
+
 # Activer l'environnement virtuel
 source .venv/bin/activate
 
 cd PicturesDjango
 
 # Lancer le serveur de développement
-gunicorn PicturesDjango.wsgi:application --bind 0.0.0.0:8000 --reload --workers 3
+gunicorn PicturesDjango.wsgi:application --bind 0.0.0.0:8000 --reload --timeout 600 --workers 3
+
 
